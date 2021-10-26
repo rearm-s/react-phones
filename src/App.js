e2e7484dc07e8231715d438ad53d58121f92a87b
Vladyslav <rearm8@gmail.com>
2021-10-26 12:50:28 +0300

    some changes

--- src/App.js
+++ src/App.js
@@ -2,20 +2,17 @@ import React from "react";
 import Header from "./components/Header";
 import {Route} from "react-router-dom";
 import Cart from "./pages/Cart";
+import Home from "./pages/Home";
 
-const Home = React.lazy(() => import('./pages/Home'))
+// const Home = React.lazy(() => import('./pages/Home'))
 
 function App() {
     return (
         <div className="wrapper">
             <Header/>
             <div className="content">
-                <Route path="/" render={() => {
-                    return <React.Suspense fallback={<div>Loading...</div>}>
-                        <Home />
-                    </React.Suspense>
-                }}  exact />
-                <Route path="/cart" component={Cart} exact/>
+                <Route path="/" exact component={Home}/>
+                <Route path="/cart" component={Cart} />
             </div>
         </div>
     );
